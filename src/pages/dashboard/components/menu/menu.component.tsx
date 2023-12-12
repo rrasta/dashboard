063a217f658bdfe136261be803555958ab3a69fb
Lukas <rasta.izdebski@gmail.com>
2023-12-12 23:52:11 +0100

    Add icons and Logo

--- src/pages/dashboard/components/menu/menu.component.tsx
+++ src/pages/dashboard/components/menu/menu.component.tsx
@@ -1,7 +1,50 @@
 import React from 'react'
 
-import { MenuStyled } from './menu.style.tsx'
+import { BsHandbag } from 'react-icons/bs'
+import { CiSettings } from 'react-icons/ci'
+import { FiUsers, FiFolderMinus } from 'react-icons/fi'
+import { IoMdLogOut } from 'react-icons/io'
+import { IoChatbubbleEllipsesOutline } from 'react-icons/io5'
+import { TbCategory } from 'react-icons/tb'
+
+import {
+  MenuStyled,
+  Logo,
+  MenuItem,
+  MenuList,
+  LogoutButton
+} from './menu.style.tsx'
 
 export const Menu: React.FC = () => {
-  return <MenuStyled>menu</MenuStyled>
+  return (
+    <MenuStyled>
+      <Logo>
+        <img src="Metrix.png" alt="logo"></img>
+      </Logo>
+      <MenuList>
+        <MenuItem>
+          <TbCategory /> Dashboard
+        </MenuItem>
+        <MenuItem>
+          <BsHandbag /> Orders
+        </MenuItem>
+        <MenuItem>
+          <FiUsers /> Customers
+        </MenuItem>
+        <MenuItem>
+          <FiFolderMinus /> Inventory
+        </MenuItem>
+        <MenuItem>
+          <IoChatbubbleEllipsesOutline /> Conversations
+        </MenuItem>
+        <MenuItem>
+          <CiSettings /> Settings
+        </MenuItem>
+      </MenuList>
+
+      <LogoutButton>
+        <IoMdLogOut /> Logout
+      </LogoutButton>
+    </MenuStyled>
+  )
 }
--- src/pages/dashboard/components/menu/menu.style.tsx
+++ src/pages/dashboard/components/menu/menu.style.tsx
@@ -1,8 +1,53 @@
 import styled from '@emotion/styled'
 
 export const MenuStyled = styled('nav')`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   padding: 10px;
-
-  background: #bd1414;
-  width: 4rem;
+  background: #ffffff;
+  width: 296px;
 `
+
+export const Logo = styled('div')`
+  margin-bottom: 60px;
+  display: flow;
+`
+
+export const MenuList = styled('ul')`
+  list-style: none;
+  padding: 0;
+  width: 100%;
+    display: compact;
+`
+
+export const MenuItem = styled('li')`
+  font-family: Poppins;
+  width: 80%;
+  padding: 10px;
+  cursor: revert;
+  display: flex;
+  align-items: center;
+  font-size: 1.2em;
+
+  &:hover {
+    background-color: #9ac4fc;
+    width: 80%;
+      border-radius: 10px;
+  }
+
+  svg {
+    margin-right: 10px;
+  }
+`
+
+export const LogoutButton = styled(MenuItem)`
+    margin-top: 30rem;
+  background: transparent;
+
+  &:hover {
+    background-color: #d491fc;
+  }
+`
+
+// Dodaj odpowiednie kolory, marginesy i inne style według preferencji.
