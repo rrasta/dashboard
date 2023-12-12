56849bfcc3ba66e54b43c64dccd179aa794d8116
Lukas <rasta.izdebski@gmail.com>
2023-12-12 20:47:41 +0100

    Add fonts

--- src/pages/dashboard/pages/index/components/example/example.component.tsx
+++ src/pages/dashboard/pages/index/components/example/example.component.tsx
@@ -3,5 +3,5 @@ import React from 'react'
 import { ExampleStyled } from './example.style.tsx'
 
 export const Example: React.FC = () => {
-  return <ExampleStyled>Example component</ExampleStyled>
+  return <ExampleStyled>Example componentt</ExampleStyled>
 }
