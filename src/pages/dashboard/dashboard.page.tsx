96e9fb203320cb81ea4ac25855c6340b0c987cde
Piotr Pliszczyński <piotr.pilczynski@frontegg.com>
2023-12-12 19:50:29 +0100

    Content Styled

--- src/pages/dashboard/dashboard.page.tsx
+++ src/pages/dashboard/dashboard.page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import { Menu } from './components/menu/menu.component.tsx'
+import { ContentStyled } from './dashboard.style.tsx'
 import { MainLayout } from './layout/main/main.layout.tsx'
 import { IndexPage } from './pages/index/index.page.tsx'
 
@@ -10,9 +11,11 @@ export const DashboardPage: React.FC = () => {
   return (
     <MainLayout>
       <Menu />
-      <Routes>
-        <Route index element={<IndexPage />} />
-      </Routes>
+      <ContentStyled>
+        <Routes>
+          <Route index element={<IndexPage />} />
+        </Routes>
+      </ContentStyled>
     </MainLayout>
   )
 }
