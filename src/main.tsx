89fc41d0b2579eac45768cc740e9b9ae72f5fb59
SanatRasta <73519407+rrasta@users.noreply.github.com>
2023-12-19 14:56:42 +0100

    Add fonts (#2)
    * Add fonts
    
    * Delete package-lock
    Add font by yarn and import to main.tsx.
    Add font-family to index.css
    
    * Fix yarn.lock and package.json
    
    * Add Generic font
    
    ---------
    
    Co-authored-by: Piotr Pliszczyński <piotr.pilczynski@frontegg.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { DashboardPage } from './pages/dashboard/dashboard.page.tsx'
 
+import { DashboardPage } from './pages/dashboard/dashboard.page.tsx'
+import '@fontsource/poppins'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
