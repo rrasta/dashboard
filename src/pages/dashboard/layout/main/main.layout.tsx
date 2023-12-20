bc23bd58944f86d6ecf5bf519c33751813d58707
Rasta <rasta.izdebski@gmail.com>
2023-12-20 15:21:36 +0100

    split MainLayout to 2 columns Add colour to Index

--- src/pages/dashboard/layout/main/main.layout.tsx
+++ src/pages/dashboard/layout/main/main.layout.tsx
@@ -1,3 +1,8 @@
 import styled from '@emotion/styled'
 
-export const MainLayout = styled('div')``
+export const MainLayout = styled('div')`
+  display: grid;
+  grid-template-columns: 296px 1fr;
+  height: 100%;
+  background-color: var(--ui-color-background-light);
+`
