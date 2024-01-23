5e5e1238a5c3639d23698b2c1129a990bb5e53b4
Piotr Pliszczyński <fookoo@gmail.com>
2024-01-23 12:06:21 +0100

    Add usage of topbar component into index page

--- src/pages/dashboard/pages/index/index.page.tsx
+++ src/pages/dashboard/pages/index/index.page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import { Example } from './components/example/example.component.tsx'
+import { TopBar } from '../../../../components/top-bar/top-bar.component.tsx'
 
 export const IndexPage: React.FC = () => {
   return (
     <div>
+      <TopBar title="Home page" />
       <h1>index page</h1>
       <Example />
     </div>
