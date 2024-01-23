fe8d54507952999e0091cb527ec3112b1fdaf98f
Piotr Pliszczyński <fookoo@gmail.com>
2024-01-23 12:33:04 +0100

    Add ArrowDown icon

--- src/components/icons/icons.component.tsx
+++ src/components/icons/icons.component.tsx
@@ -17,3 +17,21 @@ export const BellIcon = () => (
     />
   </svg>
 )
+
+export const ArrowDown = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="25"
+    height="24"
+    viewBox="0 0 25 24"
+    fill="none"
+  >
+    <path
+      d="M6.5 9L12.5 15L18.5 9"
+      stroke="#5E6366"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
