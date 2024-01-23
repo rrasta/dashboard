816697ad482f81879ba686fac57d83e596cbf176
Piotr Pliszczyński <fookoo@gmail.com>
2024-01-23 12:36:01 +0100

    Add Inter font

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import { DashboardPage } from './pages/dashboard/dashboard.page.tsx'
-import '@fontsource/poppins'
+import '@fontsource/poppins/400.css'
+import '@fontsource/poppins/500.css'
+import '@fontsource/inter/400.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
