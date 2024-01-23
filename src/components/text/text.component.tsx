a9a4484f57d2ef47db74c4999609660a74287b42
Piotr Pliszczyński <fookoo@gmail.com>
2024-01-23 12:05:14 +0100

    Add text component - h3

---
+++ src/components/text/text.component.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+
+export const H3 = styled('h3')`
+  color: var(--ui-color-black-60);
+
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`
