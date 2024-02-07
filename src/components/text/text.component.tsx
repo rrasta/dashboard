de33c05fc2a4723ae31f6fc6fb460dbb8d35724f
Lukas <rasta.izdebski@gmail.com>
2024-02-07 11:19:35 +0100

    use variable

--- src/components/text/text.component.tsx
+++ src/components/text/text.component.tsx
@@ -5,7 +5,7 @@ import { InputVariant } from '../../types/common.type.ts'
 export const H3 = styled('h3')`
   color: var(--ui-color-black-60);
 
-  font-size: 20px;
+    font-size: var(--font-size-h3);
   font-style: normal;
   font-weight: 500;
   line-height: normal;
@@ -16,9 +16,8 @@ export const InputPlaceholder = styled('div')<{ variant: InputVariant }>`
     ${({ variant }) =>
       variant === 'primary' ? '--ui-color-black-2' : '--ui-color-black-100'}
   );
-  font-family: Inter, serif;
-
-  font-size: 16px;
+    font-family: var(--font-family-default);
+    font-size: var(--font-size-default);
   font-style: normal;
   font-weight: 400;
   line-height: normal;
