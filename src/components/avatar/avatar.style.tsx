3050836b5535b9a57da149458488606f2845d61f
Lukas <rasta.izdebski@gmail.com>
2024-02-07 11:16:20 +0100

    I removed unnecessary code

--- src/components/avatar/avatar.style.tsx
+++ src/components/avatar/avatar.style.tsx
@@ -2,8 +2,5 @@ import styled from '@emotion/styled'
 
 export const AvatarStyled = styled('img')`
   border-radius: var(--ui-border-radius-sm);
-  width: 32px;
-  height: 32px;
-
-  background-color: #45464e;
+  
 `
